ff9bc3a133d7e7ace1d17af6686efa8b50948795
danrpinho <up201505302@fe.up.pt>
2017-10-03 11:20:06 +0100

    Worked on myTriangle, only thing left to do is to implement the texCoords thing

--- scene/MyTriangle.js
+++ scene/MyTriangle.js
@@ -1,297 +1,60 @@
 /**
- * MyCylinder
+ * MyObject
+ * @param gl {WebGLRenderingContext}
  * @constructor
  */
- 
-var degToRad = Math.PI / 180.0;
 
- function MyCylinder(scene, slices, stacks) {
- 	CGFobject.call(this,scene);
+function MyTriangle(scene, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+	CGFobject.call(this, scene);
 	
-	this.slices = slices;
-	this.stacks = stacks;
-
- 	this.initBuffers();
- };
-
- MyCylinder.prototype = Object.create(CGFobject.prototype);
- MyCylinder.prototype.constructor = MyCylinder;
-
- MyCylinder.prototype.initBuffers = function() {
- 	/*
- 	* TODO:
- 	* Replace the following lines in order to build a prism with a **single mesh**.
- 	*
- 	* How can the vertices, indices and normals arrays be defined to
- 	* build a prism with varying number of slices and stacks?
- 	*/
-
-
-	this.vertices = [];
-	this.normals = [];
-	this.indices = [];
-	this.texCoords = [];
-
-	var anguloFatia = 360/this.slices * degToRad; 
-
-
-	var x1,y1,z1,x2,y2,z2;
-
-	var ang = 0;
-	var indice1 = 0;
-
-	for(face = 0; face < this.slices ; face++){
-	ang = anguloFatia*face;
-	x1 = Math.cos(ang);
-	y1 = Math.sin(ang);
-	z1 = 0.0;
-	x2 = Math.cos(ang+anguloFatia);
-	y2 = Math.sin(ang+anguloFatia);
-	z2 = 0.0;
-
-	xN1 = Math.cos(ang+anguloFatia/2);
-	yN1 = Math.sin(ang+anguloFatia/2);
-	zN1 = 0.0;
-	N1 = Math.sqrt(xN1*xN1 + yN1*yN1);
-	var zed = 1/this.stacks;
-	for(z1 = 0; z1 <=1 ; z1+= 1/this.stacks){
-
-		//ponto 0
-		this.vertices.push(x1);
-		this.vertices.push(y1);
-		this.vertices.push(z1);
-		this.normals.push(x1);
-		this.normals.push(y1);
-		this.normals.push(0.0);
-
-		//this.texCoords.push(1,1);
-		this.texCoords.push(face/this.slices,1-z1);
-		this.indices.push(indice1);
-		indice1++;
-
-		//ponto 2
-		this.vertices.push(x2);
-		this.vertices.push(y2);
-		this.vertices.push(z1+zed);
-		this.normals.push(x2);
-		this.normals.push(y2);
-		this.normals.push(0.0);
-
-		//this.texCoords.push(0,0);
-		this.texCoords.push((face+1)/this.slices,1-z1-zed);
-		this.indices.push(indice1);
-		indice1++;
-
-		// ponto 1
-		this.vertices.push(x1);
-		this.vertices.push(y1);
-		this.vertices.push(z1+zed);
-
-		this.normals.push(x1);
-		this.normals.push(y1);
-		this.normals.push(0.0);
-
-		//this.texCoords.push(0,1);
-		this.texCoords.push(face/this.slices,1-z1-zed);
-		this.indices.push(indice1);
-
-		//////////////////////////////////////////////////////////////////
-		//PARA SE VER DE DENTRO PARA FORA
-		this.indices.push(indice1);
-		this.indices.push(indice1-1);
-		this.indices.push(indice1-2);
-		indice1++;
-		///////////////////////////////////////////////////////////////////
-
-		//ponto 3
-		this.vertices.push(x2);
-		this.vertices.push(y2);
-		this.vertices.push(z1);
-		this.normals.push(x2);
-		this.normals.push(y2);
-		this.normals.push(0.0);
-
-		//this.texCoords.push(1,0);
-		this.texCoords.push((face+1)/this.slices,1-z1);
-		this.indices.push(indice1);
-		indice1++;
-
-		//ponto 2
-		this.vertices.push(x2);
-		this.vertices.push(y2);
-		this.vertices.push(z1+zed);
-		this.normals.push(x2);
-		this.normals.push(y2);
-		this.normals.push(0.0);
-
-		//this.texCoords.push(0,0);
-		this.texCoords.push((face+1)/this.slices,1-z1-zed);
-		this.indices.push(indice1);
-		indice1++;
-
-		//ponto 0
-		this.vertices.push(x1);
-		this.vertices.push(y1);
-		this.vertices.push(z1);
-		this.normals.push(x1);
-		this.normals.push(y1);
-		this.normals.push(0.0);
-
-		//this.texCoords.push(1,1);
-		this.texCoords.push(face/this.slices,1-z1);
-		this.indices.push(indice1);
-
-
-
-		//////////////////////////////////////////////////////////////
-		//PARA SE VER DE DENTRO PA FORA
-		this.indices.push(indice1);
-		this.indices.push(indice1-1);
-		this.indices.push(indice1-2);
-		indice1++;
-		///////////////////////////////////////////////////////////////
-		}
-
-
-	}
-
-
-
-
-
-// 	this.vertices = [];
-// 	this.normals = [];
-// 	this.indices = [];
-// 	var anguloFatia = 360/this.slices * degToRad;
-
-// // 	this.vertices.push(0.0);
-// // 	this.vertices.push(0.0);
-// // 	this.vertices.push(0.0);
-
-// 	for(zcol = 0.0 ; zcol < this.stacks; zcol+=(1/this.stacks)){
-// 	var xAntigo = 1.0;
-// 	var yAntigo = 0.0;
-// 	var zAntigo = zcol;
-
-// 	var xNovo, yNovo, zNovo;
-// 	var a,b,c;
-// 	var normaVetor;
-// 	var xNorma;
-// 	var yNorma;
-// 	var zNorma;
-
-// 	//face dianteira
-// 	for(i = 1; i <= this.slices ; i++){
-// 		xNovo = Math.cos(i*anguloFatia);
-// 		yNovo = Math.sin(i*anguloFatia);
-// 		zNovo = zcol;
-
-// 		this.vertices.push(xAntigo);
-// 		this.vertices.push(yAntigo);
-// 		this.vertices.push(zAntigo);
-
-// 		this.vertices.push(xNovo);
-// 		this.vertices.push(yNovo);
-// 		this.vertices.push(zNovo);
-
-// // 		a = xNovo - xAntigo;
-// // 		b = yNovo - yAntigo;
-// // 		c = zcol;
-
-// // 		normaVetor = Math.sqrt(b*b + a*a);
-// // 		xNorma = b/normaVetor;
-// // 		yNorma = -1*a /normaVetor;
-// // 		zNorma = 0.0;
-
-// 		normaVetor = Math.sqrt(xAntigo*xAntigo + yAntigo*yAntigo + zAntigo*zAntigo);
-// 		xNorma = xAntigo/normaVetor;
-// 		yNorma = yAntigo/normaVetor;
-// 		zNorma = zAntigo/normaVetor;
-
-// 		this.normals.push(xNorma);
-// 		this.normals.push(yNorma);
-// 		this.normals.push(zNorma);
-
-// 		normaVetor = Math.sqrt(xNovo*xNovo + yNovo*yNovo + zNovo*zNovo);
-// 		xNorma = xNovo/normaVetor;
-// 		yNorma = yNovo/normaVetor;
-// 		zNorma = zNovo/normaVetor;
-
-// 		this.normals.push(xNorma);
-// 		this.normals.push(yNorma);
-// 		this.normals.push(zNorma);
-
-// 		xAntigo = xNovo;
-// 		yAntigo = yNovo;
-// 		zAntigo = zNovo;
-// 	}
-
-// 	//face traseira
-// 	var xAntigo = 1.0;
-// 	var yAntigo = 0.0;
-// 	var zAntigo = zcol+0.5;
-// 	for(i = 1; i <= this.slices ; i++){
-// 		xNovo = Math.cos(i*anguloFatia);
-// 		yNovo = Math.sin(i*anguloFatia);
-// 		zNovo = zcol+0.5;
-
-// 		this.vertices.push(xAntigo);
-// 		this.vertices.push(yAntigo);
-// 		this.vertices.push(zAntigo);
-
-// 		this.vertices.push(xNovo);
-// 		this.vertices.push(yNovo);
-// 		this.vertices.push(zNovo);
-
-// // 		a = xNovo - xAntigo;
-// // 		b = yNovo - yAntigo;
-// // 		c = zcol+0.5;
-
-// // 		normaVetor = Math.sqrt(b*b + a*a);
-// // 		xNorma = b/normaVetor;
-// // 		yNorma = -a /normaVetor;
-// // 		zNorma = 0;
-
-// 		normaVetor = Math.sqrt(xAntigo*xAntigo + yAntigo*yAntigo + zAntigo*zAntigo);
-// 		xNorma = xAntigo/normaVetor;
-// 		yNorma = yAntigo/normaVetor;
-// 		zNorma = zAntigo/normaVetor;
-
-// 		this.normals.push(xNorma);
-// 		this.normals.push(yNorma);
-// 		this.normals.push(zNorma);
-
-// 		normaVetor = Math.sqrt(xNovo*xNovo + yNovo*yNovo + zNovo*zNovo);
-// 		xNorma = xNovo/normaVetor;
-// 		yNorma = yNovo/normaVetor;
-// 		zNorma = zNovo/normaVetor;
-// 		this.normals.push(xNorma);
-// 		this.normals.push(yNorma);
-// 		this.normals.push(zNorma);
-
-// 		xAntigo = xNovo;
-// 		yAntigo = yNovo;
-// 		zAntigo = zNovo;
-// 	}
-
-// 	for(i = 0; i < this.slices; i++){
-// 		//+zcol*2*this.slices factor de ajuste pa todos
-// 		this.indices.push((2*i+2*this.slices+zcol*2*this.slices));
-// 		this.indices.push(2*i+zcol*2*this.slices);
-// 		this.indices.push(2*i+1+zcol*2*this.slices);
-
-		
-// 		this.indices.push((2*i+2*this.slices+zcol*2*this.slices));
-// 		this.indices.push(2*i+1+zcol*2*this.slices);
-// 		this.indices.push(((2*i+2*this.slices)+1+zcol*2*this.slices));
-// 	}  
-
-
-
-
-		
-// 	}
-
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
- };
+	// this.minS = 0;
+	// this.minT = 0;
+	// this.maxS = 1;
+	// this.maxT = 1;
+
+	this.initBuffers2(x1, y1, z1, x2, y2, z2, x3, y3, z3);
+};
+
+MyTriangle.prototype = Object.create(CGFobject.prototype);
+MyTriangle.prototype.constructor = MyTriangle;
+
+MyTriangle.prototype.initBuffers = function () {
+	this.initGLBuffers();
+};
+
+MyTriangle.prototype.initBuffers2 = function (x1, y1, z1, x2, y2, z2, x3, y3, z3) {
+	//variaveis para calcular a normal a partir dos vertices do triangulo
+	var a = x2-x1;
+	var b = y2-y1;
+	var c = z2-z1;
+	var d = x3-x1;
+	var e = y3-y1;
+	var f = z3-z1;
+
+	this.vertices = [
+		x1, y1, z1,
+		x2, y2, z2,
+		x3, y3, z3,
+	];
+
+	this.indices = [
+		0, 1, 2,
+	];
+
+	//TODO
+	this.texCoords = [
+		this.minS, this.maxT,
+		this.maxS, this.maxT,
+		this.minS, this.minT,
+		this.maxS, this.minT
+	];
+	this.primitiveType = this.scene.gl.TRIANGLES;
+
+	this.normals = [
+		b*f-c*e, d*c-a*f, a*e-d*b,
+		b*f-c*e, d*c-a*f, a*e-d*b,
+		b*f-c*e, d*c-a*f, a*e-d*b,
+	];
+
+	this.initGLBuffers();
+};
