523b6a0f110945b3f0355e003668ce6e8d105512
francis-andrade <up201503481@fe.up.pt>
2017-10-03 13:04:25 +0100

    Sphere done

---
+++ scene/MySphere.js
@@ -0,0 +1,38 @@
+/**
+
+ * MySphere
+
+ * @constructor
+
+ */
+
+ function MySphere(scene, radius, slices, stacks) {
+
+ 	CGFobject.call(this,scene);
+
+	
+
+	this.slices = slices;
+
+	this.stacks = stacks;
+
+	this.radius=radius;
+
+	this.sphere=new MySemiSphere(this.radius, this.slices, this.stacks);
+
+ };
+
+
+
+ MySphere.prototype = Object.create(CGFobject.prototype);
+
+ MySphere.prototype.constructor = MySphere;
+
+
+
+ MySphere.prototype.display = function() {
+	this.sphere.display();
+	this.sphere.rotate(1,0,0,Math.PI);
+ 	    
+
+ };
