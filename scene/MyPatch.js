cc1aad676659fd8855b04e58c26058db12d9b002
up201503481@FEUPSIG.FE.UP.PT <up201503481@FEUPSIG.FE.UP.PT>
2017-10-10 12:05:48 +0100

    My Patch created

--- scene/MyPatch.js
+++ scene/MyPatch.js
@@ -5,20 +5,38 @@
  
 var DEGREE_TO_RAD = Math.PI / 180;
 
- function Patch(scene) {
+ function MyPatch(scene, upoints, vpoints) {
  	CGFobject.call(this,scene);
 	
+	this.upoints=upoints;
+	this.vpoints=vpoints;
 
- 	this.initBuffers();
+	var knots1 = this.getKnotsVector(degree1); // to be built inside webCGF in later versions ()
+	var knots2 = this.getKnotsVector(degree2); // to be built inside webCGF in later versions
+		
+	var nurbsSurface = new CGFnurbsSurface(degree1, degree2, knots1, knots2, controlvertexes); // TODO  (CGF 0.19.3): remove knots1 and knots2 from CGFnurbsSurface method call. Calculate inside method.
+	getSurfacePoint = function(u, v) {
+		return nurbsSurface.getPoint(u, v);
+	};
+
+	this.obj = new CGFnurbsObject(scene, getSurfacePoint, 20, 20 );
+
  };
 
- Patch.prototype = Object.create(CGFobject.prototype);
- Patch.prototype.constructor = Patch;
-
- Patch.prototype.initBuffers = function() {
- 
+ MyPatch.prototype = Object.create(CGFobject.prototype);
+ MyPatch.prototype.constructor = MyPatch;
 
 
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
- };
+
+MyPatch.prototype.getKnotsVector = function(degree) { // TODO (CGF 0.19.3): add to CGFnurbsSurface
+	
+	var v = new Array();
+	for (var i=0; i<=degree; i++) {
+		v.push(0);
+	}
+	for (var i=0; i<=degree; i++) {
+		v.push(1);
+	}
+	return v;
+}
+
